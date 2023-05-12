1bdb9337c74f54536fa7ee803f7355f96d1ab6f7
Hamzah Nur Rohman <50015789+hamzahnr7@users.noreply.github.com>
2023-05-12 09:23:45 +0700

    Submit Assignment
    Sudah Melakukan Semua Point yang ada di assignment SQL W2

--- W4/no5.sql
+++ W4/no5.sql
@@ -64,17 +64,95 @@ values (null, (select id_cust from customer where username = 'user0'),
 'Komp. Buah Batu Regency A2 No.9 - 10 Kel. Kujangsari / Cijawura, Kujangsari, Bandung Kidul, Bandung City, West Java 40287',
 25000, 5000, 30000, 'Tunai', '2023-05-12');
 
-select * from driver where id_driver = (select id_driver from pesanan p where id_cust = 1);
+select * from driver where id_driver in (select id_driver from pesanan p where id_cust = 1);
 
 update pesanan 
-set biaya_perjalanan = 28000 
-where id_cust = (select id_cust from customer where username = 'user0');
+set biaya_aplikasi = 6000 
+where id_driver = (select id_driver  from driver where id_driver = 2);
 update pesanan 
 set total_bayar = 33000 
 where id_cust = (select id_cust from customer where username = 'user0');
 
 delete from pesanan 
-where id_cust = (select id_cust from customer where username = 'user0');
+where id_cust = (select id_cust from customer where username = 'user0')
+&& id_driver = 2;
+
+insert into pesanan 
+values (null, (select id_cust from customer where username = 'user1'), 
+(select id_driver from driver where jenis_mobil = 'Honda CRV'),  
+'Kp. Cijeungjing RT 05 / RW 01, No 44, Cipagalo, Kec. Bojongsoang, Kabupaten Bandung, Jawa Barat 40287',
+'Jl. Sukajadi No.131-139, Cipedes, Kec. Sukajadi, Kota Bandung, Jawa Barat 40162',
+67000, 5000, 72000, 'QRIS', '2023-05-12'),
+(null, (select id_cust from customer where username = 'user2'), 
+(select id_driver from driver where jenis_mobil = 'Hyundai Ionic 5'),  
+'Kp. Cijeungjing RT 05 / RW 01, No 44, Cipagalo, Kec. Bojongsoang, Kabupaten Bandung, Jawa Barat 40287',
+'Jl. Purnawarman No.13-15, Babakan Ciamis, Kec. Sumur Bandung, Kota Bandung, Jawa Barat 40117',
+49000, 5000, 54000, 'QRIS', '2023-05-12');
+
+select d.*, p3.total_bayar as tota_bayar_tertinggi from driver d 
+join pesanan p3 ON p3.id_driver = d.id_driver 
+where d.id_driver = 
+( select id_driver from pesanan p2 where p2.total_bayar = 
+(select max(p.total_bayar) from pesanan p) );
+
+insert into pesanan 
+values (null, (select id_cust from customer where username = 'user0'), 
+(select id_driver from driver where jenis_mobil = 'Kijang Inova'),  
+'Kp. Cijeungjing RT 05 / RW 01, No 44, Cipagalo, Kec. Bojongsoang, Kabupaten Bandung, Jawa Barat 40287',
+'Jl. Sukajadi No.131-139, Cipedes, Kec. Sukajadi, Kota Bandung, Jawa Barat 40162',
+62000, 5000, 67000, 'QRIS', '2023-05-12'),
+(null, (select id_cust from customer where username = 'user0'), 
+(select id_driver from driver where jenis_mobil = 'Hyundai Ionic 5'),  
+'Kp. Cijeungjing RT 05 / RW 01, No 44, Cipagalo, Kec. Bojongsoang, Kabupaten Bandung, Jawa Barat 40287',
+'Jl. Purnawarman No.13-15, Babakan Ciamis, Kec. Sumur Bandung, Kota Bandung, Jawa Barat 40117',
+44000, 5000, 49000, 'QRIS', '2023-05-12');
+insert into pesanan 
+values (null, (select id_cust from customer where username = 'user1'), 
+(select id_driver from driver where jenis_mobil = 'Kijang Inova'),  
+'Kp. Cijeungjing RT 05 / RW 01, No 44, Cipagalo, Kec. Bojongsoang, Kabupaten Bandung, Jawa Barat 40287',
+'Jl. Sukajadi No.131-139, Cipedes, Kec. Sukajadi, Kota Bandung, Jawa Barat 40162',
+62000, 5000, 67000, 'Tunai', '2023-05-12'),
+(null, (select id_cust from customer where username = 'user2'), 
+(select id_driver from driver where jenis_mobil = 'Hyundai Ionic 5'),  
+'Kp. Cijeungjing RT 05 / RW 01, No 44, Cipagalo, Kec. Bojongsoang, Kabupaten Bandung, Jawa Barat 40287',
+'Jl. Purnawarman No.13-15, Babakan Ciamis, Kec. Sumur Bandung, Kota Bandung, Jawa Barat 40117',
+44000, 5000, 49000, 'Tunai', '2023-05-12');
+
+SELECT nama_depan, nama_belakang, metode_bayar, total_uang 
+FROM (SELECT p.id_cust, p.metode_bayar, SUM(total_bayar) as total_uang
+      FROM pesanan p
+      GROUP BY p.id_cust) as p1
+JOIN customer c ON p1.id_cust = c.id_cust
+WHERE p1.id_cust = 2;
+
+SELECT d.nama_depan , d.jenis_mobil , id_cust , total_bayar  
+FROM pesanan p 
+JOIN driver d ON d.id_driver = p.id_driver 
+WHERE id_cust = 1
+UNION
+SELECT d1.nama_depan , d1.jenis_mobil , id_cust , total_bayar
+FROM pesanan p2 
+join driver d1 on d1.id_driver = p2.id_driver
+WHERE id_cust = 2;
+
+SELECT d.*
+FROM pesanan p 
+join driver d on d.id_driver = p.id_driver 
+WHERE p.id_cust  = 3
+INTERSECT
+SELECT d1.*
+FROM pesanan p1 
+join driver d1 on d1.id_driver = p1.id_driver 
+WHERE p1.id_cust  = 2;
+
+
+
+
+
+
+
+
+
 
 
 
