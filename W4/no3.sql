6054ce6690895fc746aaefe84614e7251f051d2e
Hamzah Nur Rohman <50015789+hamzahnr7@users.noreply.github.com>
2023-05-17 13:46:22 +0700

    fix some query

--- W4/no3.sql
+++ W4/no3.sql
@@ -1,8 +1,8 @@
-start transaction
-
 set autocommit=0;
 
-select * from products p
+start transaction;
+
+select * from products p;
 
 insert into products
 values ("S99_0985", "9087 Motor Balap 5", "Motorcycles", "1:10", "ApaAja", "", "9999", "45.45", "98.03"),
@@ -11,14 +11,14 @@ values ("S99_0985", "9087 Motor Balap 5", "Motorcycles", "1:10", "ApaAja", "", "
 ("S99_0988", "9087 Motor Balap 5", "Motorcycles", "1:10", "ApaAja", "", "9999", "45.45", "98.03"),
 ("S99_0989", "9087 Motor Balap 5", "Motorcycles", "1:10", "ApaAja", "", "9999", "45.45", "98.03");
 
-select * from products p
+select * from products p;
 
-select * from employees e 
+select * from employees e; 
 
 insert into employees 
 values (1122, "Hamzah", "Nur", "x1122", "email@mail.com", 1, null, "Investor");
 
-select * from employees e
+select * from employees e;
 
 commit
 
@@ -33,3 +33,5 @@ select * from employees e
 rollback to savepoint insertdata;
 
 select * from employees e
+
+commit
