9d30a637c81474cf467f77f154128445afe28d59
Hamzah Nur Rohman <50015789+hamzahnr7@users.noreply.github.com>
2023-05-26 00:13:15 +0700

    Progress

--- W5/Tugas 2/DB.php
+++ W5/Tugas 2/DB.php
@@ -6,24 +6,89 @@ class DB {
     protected $user = 'root';
     protected $pass = '';
     protected $dbname = 'pelatihan-php';
+    protected $conn;
 
-    public function connection()
+    public function __construct()
     {
         try {
-            // Membuat koneksi
-            $conn = new PDO("mysql:host=$this->hostname;dbname=$this->dbname", $this->user, $this->pass);
-        
-            // Menentukan mode error
-            // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-            // Jika koneksi berhasil, lakukan operasi database lainnya...
-            print_r($conn);
-        
-            // Menutup koneksi
-            $conn = null;
+            $this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->dbname", $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
         } catch(PDOException $e) {
-            // Menampilkan pesan kesalahan jika koneksi gagal
             die("Koneksi gagal: " . $e->getMessage());
+        }
+    }
+    public function closeConnection() {
+        $this->conn = null;
+    }
+    public function getDataPegawai()
+    {
+        $sql = "SELECT * FROM pegawai";
+        $result = $this->conn->query($sql);
+
+        if ($result) {
+            return $result->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            echo "Error: " . $this->conn->errorInfo()[2];
+            return false;
+        }
+    }
+    public function insertData($tableName, $data) {
+        try {
+            $placeholders = array();
+            $values = array();
+            foreach ($data as $key => $value) {
+                $placeholders[] = ':' . $key;
+                $values[':' . $key] = $value;
+            }
+
+            $sql = "INSERT INTO $tableName (" . implode(', ', array_keys($data)) . ") VALUES (" . implode(', ', $placeholders) . ")";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute($values);
+
+            echo "Data berhasil ditambahkan ke tabel $tableName.";
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+    public function updateData($tableName, $data, $condition) {
+        try {
+            $setClause = array();
+            $values = array();
+            foreach ($data as $key => $value) {
+                $setClause[] = $key . ' = :' . $key;
+                $values[':' . $key] = $value;
+            }
+            $sql = "UPDATE $tableName SET " . implode(', ', $setClause) . " WHERE id=$condition";
+
+            $result = $this->conn->prepare($sql);
+            $result->execute($values);
+
+            echo "Data berhasil diperbarui pada tabel $tableName.";
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+    public function createTable($tableName, $columns) {
+        try {
+            $sql = "CREATE TABLE $tableName ($columns)";
+
+            $this->conn->exec($sql);
+
+            echo "Tabel $tableName telah berhasil dibuat.";
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+    public function getColumnNames($tableName) {
+        try {
+            $sql = "SHOW COLUMNS FROM $tableName";
+
+            $result = $this->conn->query($sql);
+            $columns = $result->fetchAll(PDO::FETCH_COLUMN);
+
+            return $columns;
+        } catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
         }
     }
 }
--- W5/Tugas 2/index.php
+++ W5/Tugas 2/index.php
@@ -3,15 +3,32 @@
 include 'Kalkulator.php';
 include 'DB.php';
 
-echo "Selamat Datang";
+echo "Selamat Datang<br>";
 
 $kalkulator = new Kalkulator();
-
-$jumlah = $kalkulator->tambah(1,2);
-
-echo $jumlah;
-
+$x = 3;
+$y = 9;
+echo 'X = '.$x.'; Y = '.$y.';<br>';
+echo 'X + Y = '.$kalkulator->tambah($x,$y).'<br>';
+echo '<br>';
+echo 'X = '.$x.'; Y = '.$y.';<br>';
+echo 'X - Y = '.$kalkulator->kurang($x,$y).'<br>';
+echo '<br>';
+echo 'X = '.$x.'; Y = '.$y.';<br>';
+echo 'X * Y = '.$kalkulator->kali($x,$y).'<br>';
+echo '<br>';
+echo 'X = '.$x.'; Y = '.$y.';<br>';
+echo 'X / Y = '.$kalkulator->bagi($x,$y).'<br>';
+echo '<br>';
 $db = new DB();
 
-$conn = $db->connection();
-
+$result = $db->getDataPegawai();
+$column = $db->getColumnNames('pegawai');
+foreach ($result as $row => $col) {
+    $thead[] = $result[$row];
+    $data[] = implode("\n|\n", $col);
+}
+echo implode("\n|\n", $column).'<br>';
+foreach ($data as $d ) {
+    echo $d.'<br>';
+}
